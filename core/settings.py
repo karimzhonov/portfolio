3353661607864d54eb425b63f008d4800267c321
Khusniddin <khtkarimzhonov@gmail.com>
2021-11-04 00:01:44 +0300

    deploy

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zt(01xpen@cek%sukbijup$qahgif65m1oe=-maz&3^wbwm9sj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'kht-portfolio.herokuapp.com', 'localhost']
 
