992c3a3fe0d5d8fcf035e733649ea5356a9c628f
Khusniddin <khtkarimzhonov@gmail.com>
2021-11-04 00:15:34 +0300

    deploy

--- core/settings.py
+++ core/settings.py
@@ -161,7 +161,11 @@ CORS_ORIGIN_REGEX_WHITELIST = [
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
+
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
