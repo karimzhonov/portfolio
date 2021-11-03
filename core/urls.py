616d39c1f24c7f7b617466da76d96488b48814a7
Khusniddin <khtkarimzhonov@gmail.com>
2021-11-03 23:17:44 +0300

    deploy

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from main import urls as main_urls
 
@@ -9,7 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(main_urls)),
     path('api-auth/', include('rest_framework.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + staticfiles_urlpatterns()
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
