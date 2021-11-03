be109e31f65cb465f68f01616f2907b72a36db2c
Khusniddin <khtkarimzhonov@gmail.com>
2021-11-03 23:27:22 +0300

    deploy

--- core/urls.py
+++ core/urls.py
@@ -12,5 +12,4 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls'))
 ] + staticfiles_urlpatterns()
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
